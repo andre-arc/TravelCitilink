45a925efd25dc029404e6d638e640eec5f288465
Andre Darnius <andridarnius@gmail.com>
2018-07-26 02:29:30 +0700

    form checkout & Finalisasi

--- application/modules/dashboard/views/list_tiket.php
+++ application/modules/dashboard/views/list_tiket.php
@@ -27,11 +27,7 @@ if(!empty($result)){
                        </div>
                    
 
-                        <div class="col-md-12">
-                            <div class="form-group">
-                               <button type="submit" class="btn btn-primary pull-right">DETAIL</button>
-                            </div>
-                        </div>
+                        
                         </form>
                     </div>
                 </div>
