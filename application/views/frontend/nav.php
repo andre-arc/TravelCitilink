bf547c2a3f91dacd2aea81b57c0852a828cfb3e9
Rizwan Saputra <rizwasaputra@gmail.com>
2019-12-22 20:47:52 +0700

    menu

--- application/views/frontend/nav.php
+++ application/views/frontend/nav.php
@@ -2,11 +2,11 @@
 	<nav class="navbar navbar-static-top">
 		<div class="container">
 			<div class="navbar-header">
-				<a href="<?php echo site_url();?>" class="navbar-brand" data-html="true" data-toggle="tooltip" data-placement="bottom" title="<?php echo $MYCFG['GENERAL']['APP_NAME_LONG'].'<br />Ver.'.$MYCFG['GENERAL']['VERSION'];?>">
+				<a href="<?php echo site_url(); ?>" class="navbar-brand" data-html="true" data-toggle="tooltip" data-placement="bottom" title="<?php echo $MYCFG['GENERAL']['APP_NAME_LONG'] . '<br />Ver.' . $MYCFG['GENERAL']['VERSION']; ?>">
 					<b>
-						<i class="fa fa-newspaper-o"></i>&nbsp;<?php echo $MYCFG['GENERAL']['APP_NAME'];?>
+						<i class="fa fa-newspaper-o"></i>&nbsp;<?php echo $MYCFG['GENERAL']['APP_NAME']; ?>
 					</b>
-					<?php echo $MYCFG['GENERAL']['VERSION'];?>
+					<?php echo $MYCFG['GENERAL']['VERSION']; ?>
 				</a>
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
 					<i class="fa fa-bars"></i>
@@ -16,40 +16,48 @@
 			<div class="collapse navbar-collapse pull-left" id="navbar-collapse">
 				<ul class="nav navbar-nav">
 					<li>
-						<a href="<?php echo site_url('http://localhost/TravelCitilink/');?>">
-							<i class="fa fa-university"></i>&nbsp;Home
+						<a href="<?php echo site_url('#'); ?>">
+							<i class="fa fa-university"></i>&nbsp;KAPAL
 						</a>
 					</li>
 					<li class="dropdown">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="fa fa-bar-chart"></i>&nbsp;Rute <span class="caret"></span>
+							<i class="fa fa-bar-chart"></i>&nbsp; Hotel
 						</a>
-						
+
 					</li>
-					
-					<li>
-						<a href="<?php echo site_url('home/comments/');?>">
-							<i class="fa fa-comments"></i>&nbsp;Contact
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							<i class="fa fa-bar-chart"></i>&nbsp; Tour
 						</a>
+
 					</li>
-					
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							<i class="fa fa-bar-chart"></i>&nbsp; Event
+						</a>
+
+					</li>
+
+
 				</ul>
-				<!--form class="navbar-form navbar-left" role="search" method="POST" action="<?php //echo site_url('news/search/');?>">
+				<!--form class="navbar-form navbar-left" role="search" method="POST" action="<?php //echo site_url('news/search/');
+																								?>">
 					<div class="form-group">
 						<input type="text" class="form-control" name="keywords" id="keywords" placeholder="Keywords Search">
 					</div>
 					<button type="submit" class="btn btn-default" style="background-color:#000;color:#FFF;"><i class="fa fa-search"></i>&nbsp;Search</button>
-				</form-->				
+				</form-->
 			</div>
 
 			<div class="navbar-custom-menu">
 				<ul class="nav navbar-nav">
 					<li>
-						<?php if ($this->ion_auth->logged_in()):?>
-						<a href="<?php echo site_url('dashboard/');?>"><i class="fa fa-cogs"></i>&nbsp;Dashboard</a>
-						<?php else:?>
-						<a href="<?php echo site_url('acl/login/');?>"><i class="fa fa-power-off"></i>&nbsp;Login</a>
-						<?php endif;?>
+						<?php if ($this->ion_auth->logged_in()) : ?>
+							<a href="<?php echo site_url('dashboard/'); ?>"><i class="fa fa-cogs"></i>&nbsp;Dashboard</a>
+						<?php else : ?>
+							<a href="<?php echo site_url('acl/login/'); ?>"><i class="fa fa-power-off"></i>&nbsp;Login</a>
+						<?php endif; ?>
 					</li>
 				</ul>
 			</div>
