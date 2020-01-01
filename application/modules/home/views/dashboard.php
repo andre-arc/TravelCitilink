8cc6d813921a1506e8f2e6b1e1478df7ea4cf541
Rizwan Saputra <rizwasaputra@gmail.com>
2020-01-01 16:36:25 +0700

    paket

--- application/modules/home/views/dashboard.php
+++ application/modules/home/views/dashboard.php
@@ -29,7 +29,7 @@
 
             <div class="col-lg-3 col-md-6 hero-feature">
                   <div class="thumbnail">
-                        <img src="http://placehold.it/800x500" alt="">
+                        <img src="<?php echo base_url('/assets/image/paket1.jpg') ?>" alt="">
                         <div class="caption">
                               <h3>Feature Label</h3>
                               <p>This would be a great spot to feature some brand new products!</p>
@@ -41,7 +41,7 @@
 
             <div class="col-lg-3 col-md-6 hero-feature">
                   <div class="thumbnail">
-                        <img src="http://placehold.it/800x500" alt="">
+                        <img src="<?php echo base_url('/assets/image/paket2.jpg') ?>" alt="">
                         <div class="caption">
                               <h3>Feature Label</h3>
                               <p>This would be a great spot to feature some brand new products!</p>
@@ -53,7 +53,7 @@
 
             <div class="col-lg-3 col-md-6 hero-feature">
                   <div class="thumbnail">
-                        <img src="http://placehold.it/800x500" alt="">
+                        <img src="<?php echo base_url('/assets/image/paket3.jpg') ?>" alt="">
                         <div class="caption">
                               <h3>Feature Label</h3>
                               <p>This would be a great spot to feature some brand new products!</p>
@@ -65,7 +65,7 @@
 
             <div class="col-lg-3 col-md-6 hero-feature">
                   <div class="thumbnail">
-                        <img src="http://placehold.it/800x500" alt="">
+                        <img src="<?php echo base_url('/assets/image/paket4.jpg') ?>" alt="">
                         <div class="caption">
                               <h3>Feature Label</h3>
                               <p>This would be a great spot to feature some brand new products!</p>
