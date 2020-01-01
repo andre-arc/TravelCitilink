709339a0184612db2da1df31a2b6262018a1f7d6
Rizwan Saputra <rizwasaputra@gmail.com>
2020-01-01 23:46:45 +0700

    Redesain List Tiket

--- application/modules/home/views/list_tiket.php
+++ application/modules/home/views/list_tiket.php
@@ -1,97 +1,99 @@
 <div class="container" style="padding:15px 15px; background-color:#FFF !important;">
-      <section class="content">
-            <div class="row">
-                  <div class="col-md-12">
-                  <?php
-                        if(!empty($result)){
-                            echo "<form action='".base_url('transaksi/checkout')."' method='GET' id='form-checkout'>";
-                            echo form_hidden('choose', '');
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                if (!empty($result)) {
+                    echo "<form action='" . base_url('transaksi/checkout') . "' method='GET' id='form-checkout'>";
+                    echo form_hidden('choose', '');
 
-                            foreach ($result as $r) {
-                                ?>
-                                <div class="panel">
+                    foreach ($result as $r) {
+                ?>
+                        <div class="panel">
                             <div class="panel-body">
                                 <div class="row">
-                                            <div class="col-md-12">
-                                            <div class="col-md-4">
-                                                <img style="margin-left: 10%;" src="https://2.bp.blogspot.com/-kcAFN1pdt2g/VgjjMoip7wI/AAAAAAAAItU/XOkXvNtFe1c/s1600/logo%2Bmaskapai%2Bpenerbangan%2Bcitilink%2Bindonesia.JPG" alt="logo" height="100">
-                                            </div>
-                                            <div class="col-md-4 detail-tiket" >
-                                                [<?= $r->dari."-".$r->tujuan ?>]
-                                                <br>
-                                                <span class="waktu">
-                                                Keberangkatan : <?= $r->dari."-".$r->tujuan ?>
+                                    <div class="col-md-12">
+                                        <div class="col-md-3" style="text-align: center;">
+                                            <img style="margin-left: 10%;margin-top: -22px;margin-bottom: -18px;" src="<?php echo base_url('/assets/image/expressbahari.png'); ?>" alt="logo">
+                                            <h4><?= $r->kapal; ?></h4>
+                                            <h5 class="text-center">Executive</h5>
+                                        </div>
+                                        <div class="col-md-6 detail-tiket">
+                                            [<?= $r->dari . "-" . $r->tujuan ?>]
+                                            <br>
+                                            <span class="waktu">
+                                                <?= $r->waktu  ?>
                                                 <br>
                                                 Tanggal Berangkat : <?= $r->tgl_berangkat ?>
-                                                </span>
-                                            </div>
-                                            <div class="col-md-2">
-                                                <span class="harga">IDR <?= convertToRupiah($r->hrg_tiket) ?></span>
+                                            </span>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="harga">
+                                                <span><?= convertToRupiah($r->hrg_tiket) ?></span>
                                             </div>
 
-                                            <div class="col-md-2">
-                                                <button type="button" class="btn btn-success btn-lg tiket_btn" btn-id="<?= $r->id_tiket ?>">PILIH</button>
-                                            </div>
-                                                
-                                                </form>
+                                            <div class="hrgbutton">
+                                                <button type="button" class="btn bg-orange margin pull-right" btn-id="<?= $r->id_tiket ?>">PILIH</button>
                                             </div>
                                         </div>
+
+
+
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
 
-                        
+
 
 
                         <?php
-                            }
-                            echo form_close();
-                        }else{
-                            ?>
-                            <div class="panel">
-                                <div class="panel-body">
-                                    <div class="row">
-                                        <div class="col-md-12 text-center">
-                                            Tidak ada Data
-                                        </div>
-                                    </div>      
+                    }
+                    echo form_close();
+                } else {
+                        ?>
+                        <div class="panel">
+                            <div class="panel-body">
+                                <div class="row">
+                                    <div class="col-md-12 text-center">
+                                        Tidak ada Data
+                                    </div>
                                 </div>
                             </div>
-                            <?php
-                        }
+                        </div>
+                    <?php
+                }
 
-                        ?>
-                  </div>
+                    ?>
+                        </div>
 
             </div>
-      </section>
+    </section>
 
 </div>
 
 
 <script>
-
-    function check(){
-        var data = $('input.checked').map(function(){
-             return $(this).val();
+    function check() {
+        var data = $('input.checked').map(function() {
+            return $(this).val();
         });
         // console.log(data);
-        
+
         // var form_data=$("#form-checkout").serialize();
         // console.log(form_data);
     }
 
-    $(document).ready(function(){
-        $('input.check').on('click', function(){
+    $(document).ready(function() {
+        $('input.check').on('click', function() {
             $(this).toggleClass('checked')
         });
-        
-        $('.tiket_btn').click(function(e){
+
+        $('.tiket_btn').click(function(e) {
             e.preventDefault();
             $('input[name="choose"]').val($(this).attr('btn-id'));
             $('#form-checkout').submit();
         });
 
     })
-
-    
-
 </script>
