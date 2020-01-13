6874826714da219526cd47b60fb78be04898002b
Rizwan Saputra <rizwasaputra@gmail.com>
2020-01-13 12:27:44 +0700

    change ACL dan login

--- application/views/backend/foot.php
+++ application/views/backend/foot.php
@@ -1,9 +1,9 @@
 <footer class="main-footer">
 	<div class="pull-right hidden-xs">
-		<b><?php echo $MYCFG['GENERAL']['APP_NAME'];?></b><?php echo $MYCFG['GENERAL']['VERSION'];?>
-    </div>
-    <strong>
-			&copy;2017 <a href="<?php echo $MYCFG['OFFICE']['URL'];?>" target="_blank"><?php echo $MYCFG['OFFICE']['NAME'].' '.$MYCFG['OFFICE']['CITY'];?></a>.
-			</strong>
-	</footer>
-	<?php echo (isset($js)) ? $js : '';?>
+		<b><?php echo $MYCFG['GENERAL']['APP_NAME']; ?></b><?php echo $MYCFG['GENERAL']['VERSION']; ?>
+	</div>
+	<strong>
+		&copy;2020 <a href="<?php echo $MYCFG['OFFICE']['URL']; ?>" target="_blank"><?php echo $MYCFG['OFFICE']['NAME'] . ' ' . $MYCFG['OFFICE']['CITY']; ?></a>.
+	</strong>
+</footer>
+<?php echo (isset($js)) ? $js : ''; ?>
