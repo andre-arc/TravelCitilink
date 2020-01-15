6fd38abaee148f6da2dba3861547e98417dea7fa
Andre Darnius <andridarnius@gmail.com>
2020-01-15 15:37:30 +0700

    update

--- application/modules/transaksi/controllers/Transaksi.php
+++ application/modules/transaksi/controllers/Transaksi.php
@@ -6,7 +6,7 @@ class Transaksi extends MY_Controller
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->model("M_transaksi");
+		$this->load->model("M_Transaksi");
 	}
 
 	function index()
