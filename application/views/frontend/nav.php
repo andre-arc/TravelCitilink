dfc8df4e63a8d1bb620f3546532f53ac77975880
rizwan saputra <rizwansaputra@gmail.com>
2020-07-09 23:30:42 +0700

    configdb

--- application/views/frontend/nav.php
+++ application/views/frontend/nav.php
@@ -3,7 +3,7 @@
 		<div class="container" style="margin-bottom: 10px;margin-top: 10px;">
 			<div class="navbar-header">
 				<a href="<?php echo site_url(); ?>" class="navbar-brand" data-html="true">
-					<img src="<?php echo $this->config->item('asset_url').'assets/image/logo.png' ?>" width="120px" style="margin-top: -6px;" alt="">
+					<img src="<?php echo $this->config->item('asset_url') . 'assets/image/logo.png' ?>" width="120px" style="margin-top: -6px;" alt="">
 				</a>
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
 					<i class="fa fa-bars"></i>
@@ -13,19 +13,25 @@
 			<div class="collapse navbar-collapse pull-left" id="navbar-collapse">
 				<ul class="nav navbar-nav">
 					<li>
-						<a class="menu-label" href="<?php echo site_url('#'); ?>">
-							KAPAL
-						</a>
-					</li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							TOUR
+						<a href="https://touristix.id/" class="menu-label">
+							TOUR TRAVEL
 						</a>
 
 					</li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							EVENT
+					<li>
+						<a class="menu-label" href="<?php echo site_url('#'); ?>">
+							TIKET KAPAL
+						</a>
+					</li>
+					<li>
+						<a href="#" class="menu-label">
+							RENTAL MOBIL
+						</a>
+
+					</li>
+					<li>
+						<a href="#" class="menu-label">
+							LAINNYA
 						</a>
 
 					</li>
@@ -37,9 +43,11 @@
 
 			<div class="navbar-custom-menu">
 				<ul class="nav navbar-nav">
-					<li>
-						<a href="<?php echo site_url('home/konfirmasi/'); ?>">&nbsp;CEK ORDER</a>
-					</li>
+					<div class="btn-group" style="padding: 7px;">
+						<a href="<?php echo site_url('home/konfirmasi/'); ?>" class="btn btn-warning active">
+							CEK ORDER
+						</a>
+					</div>
 					<li>
 						<?php if ($this->ion_auth->logged_in()) : ?>
 							<a href="<?php echo site_url('dashboard/'); ?>"><i class="fa fa-cogs"></i>&nbsp;Dashboard</a>
